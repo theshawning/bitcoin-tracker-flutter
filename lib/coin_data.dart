b3c4b1a520ff80520f94af84165668d1d9477b53
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-08-09 15:14:28 +0100

    structure finished, can call api and get USD price for bitcoin adding other currencies later

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,8 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const String apiKey = 'E0457752-47C9-46BC-8B95-9CC603981F77';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +33,16 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  getCoinData() async {
+    http.Response response = await http
+        .get('https://rest.coinapi.io/v1/exchangerate/BTC/USD?apiKey=$apiKey');
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      var rate = data['rate'];
+      return rate;
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
